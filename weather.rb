b00230ea7d40644003b732be0942bea26910e8ed
kateps12 <kateps12@gmail.com>
2020-02-18 01:46:19 +0000

    week 7 stuff

--- weather.rb
+++ weather.rb
@@ -1,7 +1,7 @@
 require "forecast_io"
 
 # configure the Dark Sky API with your API key
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "34a5c5394a5b9ab52c00af35bd1959e1"
 
 # do the heavy lifting, use Global Hub lat/long
 forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
@@ -9,6 +9,31 @@ forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
 # pp = pretty print
 # use instead of `puts` to make reading a hash a lot easier
 # e.g. `pp forecast`
+
+
+current_temperature = forecast["currently"]["temperature"]
+current_conditions = forecast["currently"]["summary"]
+
+#high_temp = forecast["daily"]["data"][0]["temperatureHigh"]
+
+puts "In Chicago, it is currently #{current_temperature} and #{current_conditions}."
+puts "Extended forecast:"
+for day in forecast["daily"]["data"]
+   puts "A high temperature of #{day["temperatureHigh"]} and #{day["summary"]}"
+end
+
+
+
+#puts high_temp
+#puts "A high temperature of #{forecast["daily"]["data"][0]["temperatureHigh"]} and #{forecast["daily"]["data"][0]["summary"]}"
+#puts "A high temperature of #{forecast["daily"]["data"][1]["temperatureHigh"]} and #{forecast["daily"]["data"][1]["summary"]}"
+#puts "A high temperature of #{forecast["daily"]["data"][2]["temperatureHigh"]} and #{forecast["daily"]["data"][2]["summary"]}"
+#puts "A high temperature of #{forecast["daily"]["data"][3]["temperatureHigh"]} and #{forecast["daily"]["data"][3]["summary"]}"
+#puts "A high temperature of #{forecast["daily"]["data"][4]["temperatureHigh"]} and #{forecast["daily"]["data"][4]["summary"]}"
+#puts "A high temperature of #{forecast["daily"]["data"][5]["temperatureHigh"]} and #{forecast["daily"]["data"][5]["summary"]}"
+#puts "A high temperature of #{forecast["daily"]["data"][6]["temperatureHigh"]} and #{forecast["daily"]["data"][6]["summary"]}"
+#puts "A high temperature of #{forecast["daily"]["data"][7]["temperatureHigh"]} and #{forecast["daily"]["data"][7]["summary"]}"
+
 
 # Example output:
 #
