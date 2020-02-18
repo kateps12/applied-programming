7e4c1e8d799cc3c4e93b833870e07541bf7a62d6
kateps12 <kateps12@gmail.com>
2020-02-18 02:23:10 +0000

    dice updates

--- dice.rb
+++ dice.rb
@@ -3,6 +3,19 @@ require "sinatra/reloader"
 def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
+
+
 get "/" do
-  "Hello"
+
+@dice1 = rand(1..6)
+@dice2 = rand(1..6)
+
+view "dice"
+
+    #"Hello"
+#"Hello - Welcome to the Game of Craps! 
+#<p> <h1> #{dice1} </h1> </p>
+#<p> <h1> #{dice2} </h1> </p>
+#<p> Total = #{total}</p>"
+
 end
